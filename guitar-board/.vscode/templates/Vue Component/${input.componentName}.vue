c5d500d8f6fa9bb27ef1701cf88c9863044140d3
xinxin.chen <chxinxin370@163.com>
2024-06-14 22:57:41 +0800

    add vue template

---
+++ guitar-board/.vscode/templates/Vue Component/${input.componentName}.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  }
+})
+export default class ${input.componentName} extends Vue {
+  mounted() { 
+  }
+}
+</script>
